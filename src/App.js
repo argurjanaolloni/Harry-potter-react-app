e89c0f34f11301707d1a28722737fbb7796bd976
argurjanaolloni <argurjanaolloni@gmail.com>
2023-01-13 04:48:27 -0500

    deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+import Nav from './Nav.jsx';
 import './App.css';
 
 function App() {
+  const [characters, setCharacters] = useState([])
+  const [character, setCharacter] = useState({})
+  let [modalToggle, setModalToggle] = useState(false)
+
+  useEffect(() => {
+    apiCall()
+  }, [])
+
+  function apiCall() {
+    fetch('https://hp-api.onrender.com/api/characters/')
+    
+    .then((res) => res.json())
+    .then((data) => setCharacters(data))
+  }
+  
+
+
+  function display() {
+    setModalToggle(prev => !prev)
+  }
+
+  function handleClick(charInfo) {
+    setCharacter(charInfo)
+    display()
+  }
+
+  // const characterModal = [...characters].slice(0, 25)
+  // console.log(characters)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <div className="characters-list">
+        
+        {characters.slice(0, 25).map((character, index) => (
+          <div className="character-container" onClick={() => handleClick(character)} key={index}>
+            <img className= "image" src={character.image} />
+            <p>{character.name}</p>
+          </div>
+        ))}
+      </div>
+      {modalToggle ?
+        <div className="modal">
+          <div className="modal-content">
+            <div className="info">
+              <h1>{character.name}</h1>
+              <p>Species: {character.species}</p>
+              <p>Ancestry: {character.ancestry}</p>
+              <p>Gender: {character.gender}</p>
+              <p>Wand: {character.wand.wood}</p>
+              <p>Core: {character.wand.core}</p>
+              <p>Patronus: {character.patronus}</p>
+              <button onClick={display}>x</button>
+              </div>
+        </div>
+          </div>
+        
+        :
+        null
+      }
     </div>
   );
 }
---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+      <h1>Harry Potter Characters</h1>
+    </nav>
+  )
+}
+
+export default Nav;
